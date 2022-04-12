afca81ce6becbca8feec6915e0698d9cb58e5640
kipdurron <ishunin97@gmail.com>
2022-04-12 23:24:11 +0300

    Initial Commit

---
+++ KonturKadyrovIlya/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KonturKadyrovIlya
+//
+//  Created by Илья Кадыров on 12.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
